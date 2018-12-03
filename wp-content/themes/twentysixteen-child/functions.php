2289b2be4d2ebae225239fa245123bf34cfcd2e2
Student <student@CVS-R212D007.nic.bc.ca>
2018-12-03 11:10:20 -0800

    removes metadata and adds copyright date

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -7,3 +7,38 @@ function theme_enqueue_styles() {
        array('parent-style')
    );
 }
+
+// Removesmetadata from blogpost
+function twentysixteen_entry_meta() {
+	// $format = get_post_format();
+	// if ( current_theme_supports( 'post-formats', $format ) ) {
+	// 	printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+	// 		sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentysixteen' ) ),
+	// 		esc_url( get_post_format_link( $format ) ),
+	// 		get_post_format_string( $format )
+	// 	);
+	// }
+
+}
+
+function comicpress_copyright() {
+global $wpdb;
+$copyright_dates = $wpdb->get_results("
+SELECT
+YEAR(min(post_date_gmt)) AS firstdate,
+YEAR(max(post_date_gmt)) AS lastdate
+FROM
+$wpdb->posts
+WHERE
+post_status = 'publish'
+");
+$output = '';
+if($copyright_dates) {
+$copyright = "&copy; " . $copyright_dates[0]->firstdate;
+if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+$copyright .= '-' . $copyright_dates[0]->lastdate;
+}
+$output = $copyright;
+}
+return $output;
+}
